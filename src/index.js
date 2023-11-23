72a902520ff615adc59db191b8d394ace49fc714
Sagrado, Princess Genevieve <xhanhime@gmai.com>
2023-11-23 22:16:57 +0800

    Added push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './plugins/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./plugins/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Register service worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then((registration) => {
+      console.log("Service Worker registered with scope:", registration.scope);
+    })
+    .catch((error) => {
+      console.error("Error registering Service Worker:", error);
+    });
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-      <App />
+    <App />
   </Provider>
 );
-
 
 reportWebVitals();
---
+++ sw.js
@@ -0,0 +1,1 @@
+importScripts("https://cdn.pushalert.co/sw-64509_2.js");
