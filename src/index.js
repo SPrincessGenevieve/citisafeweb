b93c31c99d461ee4b952fae32c251e9efed57a54
Sagrado, Princess Genevieve <xhanhime@gmai.com>
2023-11-23 22:55:18 +0800

    removed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./plugins/store";
-import { initOneSignal } from "./utilities/onesignal";
-
-initOneSignal(); // Initialize OneSignal
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
