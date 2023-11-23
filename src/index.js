e32cec3fe7b75e89ea07636e398628268df6f638
Sagrado, Princess Genevieve <xhanhime@gmai.com>
2023-11-23 22:23:26 +0800

    Removed push notif

--- src/index.js
+++ src/index.js
@@ -6,18 +6,6 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./plugins/store";
 
-// Register service worker
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/sw.js")
-    .then((registration) => {
-      console.log("Service Worker registered with scope:", registration.scope);
-    })
-    .catch((error) => {
-      console.error("Error registering Service Worker:", error);
-    });
-}
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
--- sw.js
+++
@@ -1,1 +0,0 @@
-importScripts("https://cdn.pushalert.co/sw-64509_2.js");
